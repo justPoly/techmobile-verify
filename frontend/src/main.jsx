5bace60e5f73580c550bb1aa9012bd2bb9c078c2
Polycarp Atalor <polycarpatalor@gmail.com>
2026-05-14 18:11:38 +0100

    Google Analytics Integration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App.jsx';
 import { AuthProvider } from "./context/AuthContext";
 
+import ReactGA from "react-ga4";
+
+// Initialize with your Measurement ID
+ReactGA.initialize("G-RQWJMQSVCQ");
+
+// Send the initial pageview
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
